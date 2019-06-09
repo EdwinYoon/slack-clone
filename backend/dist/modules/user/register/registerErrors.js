3a7963f712b1bc74ed393bea1ca31d22faf10831
EdwinYoon <ep160101@gmail.com>
2019-06-08 23:57:00 -0400

    remove build

--- backend/dist/modules/user/register/registerErrors.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.duplicateEmailError = {
-    path: 'email',
-    message: 'The Email is already taken',
-};
-//# sourceMappingURL=registerErrors.js.map
