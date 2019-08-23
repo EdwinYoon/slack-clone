05709b4ee39586110e4305f9ab7f9696ee742e43
EdwinYoon <ep160101@gmail.com>
2019-08-23 12:10:55 -0400

    Add auth link for jwt tokens

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import { onError } from 'apollo-link-error';
 import { createHttpLink } from 'apollo-link-http';
 import { ApolloLink } from 'apollo-link';
+import { setContext } from 'apollo-link-context';
 import { render } from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import { ApolloProvider as ApolloHooksProvider } from '@apollo/react-hooks';
@@ -12,10 +13,28 @@ import App from './routes/App';
 import InitializeCSS from './InitializeCSS';
 import theme from './components/common/theme';
 
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+
+  // return the headers to the context so httpLink can read them
+  const refreshToken = localStorage.getItem('refresh-token');
+
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      'x-token': token !== null ? token : '',
+      'x-refresh-token': refreshToken !== null ? refreshToken : '',
+    },
+  };
+});
+
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors) graphQLErrors.forEach(({ message, locations, path }) => console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`));
   if (networkError) console.log(`[Network error]: ${networkError}`);
 });
+
 const httpLink = createHttpLink({
   uri: 'http://localhost:5000',
 });
@@ -23,7 +42,7 @@ const httpLink = createHttpLink({
 const cache = new InMemoryCache({});
 
 const client = new ApolloClient({
-  link: ApolloLink.from([errorLink, httpLink]),
+  link: ApolloLink.from([authLink, errorLink, httpLink]),
   cache,
 });
 
