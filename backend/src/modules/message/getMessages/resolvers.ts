0e8bd66181d58748ee5f35a6aa2bcdfe91b5314e
EdwinYoon <ep160101@gmail.com>
2019-08-25 22:10:21 -0400

    BACKEND: Add getMessages functionality

--- backend/src/modules/message/getMessages/resolvers.ts
+++ backend/src/modules/message/getMessages/resolvers.ts
@@ -1,9 +1,16 @@
+// import { createQueryBuilder } from 'typeorm';
 import { ResolverMap } from '../../../types/RevolserMap';
+import { Message } from '../../../entity';
 
 export const resolvers: ResolverMap = {
   Query: {
-    messages: () => {
-      return [];
+    messages: async (_, { channelId }) => {
+      const messages = await Message.find({
+        where: { channel: channelId },
+        relations: ['user'],
+      });
+
+      return messages;
     },
   },
 };
