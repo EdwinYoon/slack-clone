110f086957d164676a618a5049dff571b003e408
EdwinYoon <ep160101@gmail.com>
2019-08-26 09:04:39 -0400

    FRONTEND: Remove AuthLink && Add WebSocketLink

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,31 +4,17 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { onError } from 'apollo-link-error';
 import { createHttpLink } from 'apollo-link-http';
-import { ApolloLink } from 'apollo-link';
+import { ApolloLink, split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
 import { setContext } from 'apollo-link-context';
 import { render } from 'react-dom';
 import { ThemeProvider } from 'styled-components';
+import { getMainDefinition } from 'apollo-utilities';
+
 import { ApolloProvider as ApolloHooksProvider } from '@apollo/react-hooks';
 import App from './routes/App';
 import InitializeCSS from './InitializeCSS';
 import theme from './components/common/theme';
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('token');
-
-  // return the headers to the context so httpLink can read them
-  const refreshToken = localStorage.getItem('refresh-token');
-
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      'x-token': token !== null ? token : '',
-      'x-refresh-token': refreshToken !== null ? refreshToken : '',
-    },
-  };
-});
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors) graphQLErrors.forEach(({ message, locations, path }) => console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`));
@@ -39,10 +25,28 @@ const httpLink = createHttpLink({
   uri: 'http://localhost:5000',
 });
 
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:5000/',
+  options: {
+    reconnect: true,
+  },
+});
+
 const cache = new InMemoryCache({});
 
+const terminatingLink = split(
+  ({ query: { definitions } }) => definitions.some((node) => {
+    const { kind, operation } = node;
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  }),
+  wsLink,
+  httpLink,
+);
+
+const link = ApolloLink.from([errorLink, terminatingLink]);
+
 const client = new ApolloClient({
-  link: ApolloLink.from([authLink, errorLink, httpLink]),
+  link,
   cache,
 });
 
